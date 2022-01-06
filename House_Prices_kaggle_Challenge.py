232466b03c7b3b7a43e087c315d202a73693167f
Michael Jean <michaeljean7101@gmail.com>
2022-01-06 13:30:27 -0600

    Update House_Prices_kaggle_Challenge.py

--- House_Prices_kaggle_Challenge.py
+++ House_Prices_kaggle_Challenge.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 # Introduction: In this exercise, I will create and submit predictions
 # for the Housing Prices competition for Kaggle users.
 
@@ -73,46 +72,3 @@ sns.set(font_scale=1.25)
 hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={
                  'size': 10}, yticklabels=cols.values, xticklabels=cols.values)
 plt.show()
-=======
-# Import helpful libraries
-import pandas as pd
-import math
-import random
-import numpy as np
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_absolute_error
-from sklearn.model_selection import train_test_split
-
-# Load the data, and separate the target
-iowa_file_path = 'train.csv'
-home_data = pd.read_csv(iowa_file_path)
-y = home_data.SalePrice
-
-# Create X (After completing the exercise, you can return to modify this line!)
-features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath']
-
-# Select columns corresponding to features, and preview the data
-X = home_data[features]
-X.head()
-
-# Split into validation and training data
-train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
-
-# Define a random forest model
-rf_model = RandomForestRegressor(random_state=1)
-rf_model.fit(train_X, train_y)
-rf_val_predictions = rf_model.predict(val_X)
-rf_val_mae = mean_absolute_error(rf_val_predictions, val_y)
-
-print("Validation MAE for Random Forest Model: {:,.0f}".format(rf_val_mae))
-
-# test pandas
-#file = pd.read_csv('train.csv')
-#print(file.head(20))
-#pti
->>>>>>> Stashed changes
