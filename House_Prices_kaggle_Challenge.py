ebffa5c968b92af502986726beb43fe76941ab27
Michael Jean <michaeljean7101@gmail.com>
2022-01-06 14:09:23 -0600

    Update House_Prices_kaggle_Challenge.py

--- House_Prices_kaggle_Challenge.py
+++ House_Prices_kaggle_Challenge.py
@@ -23,8 +23,8 @@ print(test_data.columns)
 print(test_data['SalePrice'].describe())
 
 # histogram
-sns.distplot(test_data['SalePrice'])
-plt.show()
+# sns.distplot(test_data['SalePrice'])
+# plt.show()
 
 # skewness and kurtosis
 print("Skewness: %f" % test_data['SalePrice'].skew())
@@ -33,14 +33,14 @@ print("Kurtosis: %f" % test_data['SalePrice'].kurt())
 # scatter plot grlivarea/saleprice
 var = 'GrLivArea'
 data = pd.concat([test_data['SalePrice'], test_data[var]], axis=1)
-data.plot.scatter(x=var, y='SalePrice', ylim=(0, 800000))
-plt.show()
+# data.plot.scatter(x=var, y='SalePrice', ylim=(0, 800000))
+# plt.show()
 
 # scatter plot totalbsmtsf/saleprice
 var = 'TotalBsmtSF'
 data = pd.concat([test_data['SalePrice'], test_data[var]], axis=1)
-data.plot.scatter(x=var, y='SalePrice', ylim=(0, 800000))
-plt.show()
+# data.plot.scatter(x=var, y='SalePrice', ylim=(0, 800000))
+# plt.show()
 
 # box plot overallqual/saleprice
 var = 'OverallQual'
@@ -48,21 +48,21 @@ data = pd.concat([test_data['SalePrice'], test_data[var]], axis=1)
 f, ax = plt.subplots(figsize=(8, 6))
 fig = sns.boxplot(x=var, y="SalePrice", data=data)
 fig.axis(ymin=0, ymax=800000)
-plt.show()
+# plt.show()
 
 var = 'YearBuilt'
 data = pd.concat([test_data['SalePrice'], test_data[var]], axis=1)
 f, ax = plt.subplots(figsize=(16, 8))
 fig = sns.boxplot(x=var, y="SalePrice", data=data)
 fig.axis(ymin=0, ymax=800000)
-plt.xticks(rotation=90)
-plt.show()
+# plt.xticks(rotation=90)
+# plt.show()
 
 # correlation matrix
 corrmat = test_data.corr()
 f, ax = plt.subplots(figsize=(12, 9))
-sns.heatmap(corrmat, vmax=.8, square=True)
-plt.show()
+# sns.heatmap(corrmat, vmax=.8, square=True)
+# plt.show()
 
 # saleprice correlation matrix
 k = 10
@@ -70,5 +70,26 @@ cols = corrmat.nlargest(k, 'SalePrice')['SalePrice'].index
 cm = np.corrcoef(test_data[cols].values.T)
 sns.set(font_scale=1.25)
 hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={
-                 'size': 10}, yticklabels=cols.values, xticklabels=cols.values)
-plt.show()
+              'size': 10}, yticklabels=cols.values, xticklabels=cols.values)
+# plt.show()
+
+# scatterplot
+sns.set()
+cols = ['SalePrice', 'OverallQual', 'GrLivArea', 'GarageCars', 'TotalBsmtSF',
+        'FullBath', 'YearBuilt']
+# sns.pairplot(test_data[cols], size=2.5)
+# plt.show()
+
+# missing data
+total = test_data.isnull().sum().sort_values(ascending=False)
+percent = (test_data.isnull().sum()/test_data.isnull().count()).sort_values(
+                                                            ascending=False)
+missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+missing_data.head(20)
+
+# dealing with missing data
+test_data = test_data.drop((missing_data[missing_data['Total'] > 1]).index, 1)
+test_data = test_data.drop(
+                        test_data.loc[test_data['Electrical'].isnull()].index)
+# just checking that there's no missing data missing...
+test_data.isnull().sum().max()
