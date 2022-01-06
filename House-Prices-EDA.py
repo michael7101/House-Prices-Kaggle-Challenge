506c5d8950ad66f9d10b1971f68b74a1b1075711
Michael Jean <michaeljean7101@gmail.com>
2022-01-06 16:24:12 -0600

    Create House-Prices-EDA.py

---
+++ House-Prices-EDA.py
@@ -0,0 +1,46 @@
+# House Prices EDA
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+import sklearn.linear_model as linear_model
+import seaborn as sns
+import xgboost as xgb
+import scipy.stats as st
+from sklearn.model_selection import KFold
+
+from sklearn.manifold import TSNE
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+
+pd.options.display.max_rows = 1000
+pd.options.display.max_columns = 20
+
+train = pd.read_csv('train.csv')
+test = pd.read_csv('train.csv')
+
+quantitative = [f for f in train.columns if train.dtypes[f] != 'object']
+quantitative.remove('SalePrice')
+quantitative.remove('Id')
+qualitative = [f for f in train.columns if train.dtypes[f] == 'object']
+
+missing = train.isnull().sum()
+missing = missing[missing > 0]
+missing.sort_values(inplace=True)
+missing.plot.bar()
+plt.show()
+
+
+y = train['SalePrice']
+plt.figure(1)
+plt.title('Johnson SU')
+sns.distplot(y, kde=False, fit=st.johnsonsu)
+plt.figure(2)
+plt.title('Normal')
+sns.distplot(y, kde=False, fit=st.norm)
+plt.figure(3)
+plt.title('Log Normal')
+sns.distplot(y, kde=False, fit=st.lognorm)
+plt.show()
