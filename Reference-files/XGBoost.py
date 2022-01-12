2167d54eae2523c68ebec43799dcc65c697f5407
Michael Jean <michaeljean7101@gmail.com>
2022-01-12 16:17:16 -0600

    Update XGBoost.py

--- Reference-files/XGBoost.py
+++ Reference-files/XGBoost.py
@@ -1,106 +1,83 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
+from xgboost import XGBRegressor
 
 # Read the data
-X_full = pd.read_csv('train.csv', index_col='Id')
+X = pd.read_csv('train.csv', index_col='Id')
 X_test_full = pd.read_csv('test.csv', index_col='Id')
 
 # Remove rows with missing target, separate target from predictors
-X_full.dropna(axis=0, subset=['SalePrice'], inplace=True)
-y = X_full.SalePrice
-X_full.drop(['SalePrice'], axis=1, inplace=True)
+X.dropna(axis=0, subset=['SalePrice'], inplace=True)
+y = X.SalePrice
+X.drop(['SalePrice'], axis=1, inplace=True)
 
 # Break off validation set from training data
 X_train_full, X_valid_full, y_train, y_valid = train_test_split(
-    X_full, y, train_size=0.8, test_size=0.2, random_state=0)
+    X, y, train_size=0.8, test_size=0.2, random_state=0)
 
 # "Cardinality" means the number of unique values in a column
 # Select categorical columns with relatively low cardinality
-categorical_cols = [cname for cname in X_train_full.columns if
-                    X_train_full[cname].nunique() < 10 and
-                    X_train_full[cname].dtype == "object"]
+low_cardinality_cols = [
+    cname for cname in X_train_full.columns if X_train_full[cname].nunique() <
+    10 and X_train_full[cname].dtype == "object"]
 
-# Select numerical columns
-numerical_cols = [
+# Select numeric columns
+numeric_cols = [
     cname for cname in X_train_full.columns if X_train_full[cname].dtype in [
         'int64', 'float64']]
 
 # Keep selected columns only
-my_cols = categorical_cols + numerical_cols
+my_cols = low_cardinality_cols + numeric_cols
 X_train = X_train_full[my_cols].copy()
 X_valid = X_valid_full[my_cols].copy()
 X_test = X_test_full[my_cols].copy()
 
-# Preprocessing for numerical data
-numerical_transformer = SimpleImputer(strategy='constant')
+# One-hot encode the data (to shorten the code, we use pandas)
+X_train = pd.get_dummies(X_train)
+X_valid = pd.get_dummies(X_valid)
+X_test = pd.get_dummies(X_test)
+X_train, X_valid = X_train.align(X_valid, join='left', axis=1)
+X_train, X_test = X_train.align(X_test, join='left', axis=1)
 
-# Preprocessing for categorical data
-categorical_transformer = Pipeline(steps=[
-    ('imputer', SimpleImputer(strategy='most_frequent')),
-    ('onehot', OneHotEncoder(handle_unknown='ignore'))])
+# Build model
+# Define the model
+my_model_1 = XGBRegressor(random_state=0)
 
-# Bundle preprocessing for numerical and categorical data
-preprocessor = ColumnTransformer(
-    transformers=[
-        ('num', numerical_transformer, numerical_cols),
-        ('cat', categorical_transformer, categorical_cols)])
+# Fit the model
+my_model_1.fit(X_train, y_train)
 
-# Define model
-model = RandomForestRegressor(n_estimators=100, random_state=0)
+# Get predictions
+predictions_1 = my_model_1.predict(X_valid)
 
-# Bundle preprocessing and modeling code in a pipeline
-clf = Pipeline(steps=[('preprocessor', preprocessor),
-                      ('model', model)])
+# Calculate MAE
+mae_1 = mean_absolute_error(predictions_1, y_valid)
+print("Mean Absolute Error:", mae_1)
 
-# Preprocessing of training data, fit model
-clf.fit(X_train, y_train)
+# Improve the model
+# Define the model
+my_model_2 = XGBRegressor(n_estimators=1000, learning_rate=0.05)
 
-# Preprocessing of validation data, get predictions
-preds = clf.predict(X_valid)
+# Fit the model
+my_model_2.fit(X_train, y_train)
 
-print('MAE:', mean_absolute_error(y_valid, preds))
+# Get predictions
+predictions_2 = my_model_2.predict(X_valid)
 
-# Preprocessing for numerical data
-numerical_transformer = SimpleImputer(strategy='constant')
+# Calculate MAE
+mae_2 = mean_absolute_error(predictions_2, y_valid)
+print("Mean Absolute Error:", mae_2)
 
-# Preprocessing for categorical data
-categorical_transformer = Pipeline(steps=[
-    ('imputer', SimpleImputer(strategy='constant')),
-    ('onehot', OneHotEncoder(handle_unknown='ignore'))])
+# Break the model
+# Define the model
+my_model_3 = XGBRegressor(n_estimators=1)
 
-# Bundle preprocessing for numerical and categorical data
-preprocessor = ColumnTransformer(
-    transformers=[
-        ('num', numerical_transformer, numerical_cols),
-        ('cat', categorical_transformer, categorical_cols)])
+# Fit the model
+my_model_3.fit(X_train, y_train)
 
-# Define model
-model = RandomForestRegressor(n_estimators=100, random_state=0)
+# Get predictions
+predictions_3 = my_model_3.predict(X_valid)
 
-# Bundle preprocessing and modeling code in a pipeline
-my_pipeline = Pipeline(steps=[('preprocessor', preprocessor),
-                              ('model', model)])
-
-# Preprocessing of training data, fit model
-my_pipeline.fit(X_train, y_train)
-
-# Preprocessing of validation data, get predictions
-preds = my_pipeline.predict(X_valid)
-
-# Evaluate the model
-score = mean_absolute_error(y_valid, preds)
-print('MAE:', score)
-
-# Preprocessing of test data, fit model
-preds_test = my_pipeline.predict(X_test)
-
-# Save test predictions to file
-output = pd.DataFrame({'Id': X_test.index,
-                       'SalePrice': preds_test})
-output.to_csv('submission3.csv', index=False)
+# Calculate MAE
+mae_3 = mean_absolute_error(predictions_3, y_valid)
+print("Mean Absolute Error:", mae_3)
